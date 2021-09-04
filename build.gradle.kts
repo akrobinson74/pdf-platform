cd187f472a66a08c33a54bb8dc16c61cbfdc88e7
Adam Robinson <adam.robinson@issgovernance.com>
2021-09-04 12:33:25 +0200

    + switched to AdoptOpenJDK 16.0.1-hs + eliminated redundant graphql-java-tools dependency graphql-java is already imported by graphql-spring-boot-starter + fixed test context issue by specifying webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT in test annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
+    jacoco
+    `java-library`
     id("org.springframework.boot") version "2.5.4"
     id("io.freefair.lombok") version "6.1.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("java-library")
     id("java")
 }
 
@@ -24,24 +25,26 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core:2.12.5")
     // https://mvnrepository.com/artifact/com.google.guava/guava
     implementation("com.google.guava:guava:30.1.1-jre")
-    implementation("com.graphql-java-kickstart:graphql-java-tools:11.1.1")
+//    implementation("com.graphql-java-kickstart:graphql-java-tools:11.1.1") {
+//        exclude("com.graphql-java", "graphql-java")
+//    }
     implementation("com.graphql-java-kickstart:graphiql-spring-boot-starter:11.1.0")
     implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:11.1.0")
-    implementation("com.graphql-java-kickstart:graphql-webclient-spring-boot-starter:1.0.0")
-    implementation("com.graphql-java-kickstart:voyager-spring-boot-starter:11.1.0")
+//    implementation("com.graphql-java-kickstart:graphql-webclient-spring-boot-starter:1.0.0")
+//    implementation("com.graphql-java-kickstart:voyager-spring-boot-starter:11.1.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
+//    implementation("org.springframework.boot:spring-boot-starter-websocket")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("com.graphql-java-kickstart:graphql-spring-boot-starter-test:11.1.0")
+//    testImplementation("com.graphql-java-kickstart:graphql-spring-boot-starter-test:11.1.0")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.compileJava {
-    options.compilerArgs = listOf("-Xlint:unchecked")
+    options.compilerArgs = listOf("-Xlint:deprecation","-Xlint:unchecked")
 }
 
 tasks.test {
--- src/test/java/com/simfund/graphql/pdfPlatform/ApplicationTests.java
+++ src/test/java/com/simfund/graphql/pdfPlatform/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.simfund.graphql.pdfPlatform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
 	@Test
